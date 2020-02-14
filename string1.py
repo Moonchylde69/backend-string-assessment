3030d1b5ec0341f1a6889d71695d869b8e26e694
Paige Adams <padams87@icloud.com>
2020-02-13 22:13:08 -0500

    just finished code in py3

--- string1.py
+++ string1.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Kenzie assignment: String1
 """
@@ -26,8 +26,9 @@ Kenzie assignment: String1
 
 
 def donuts(count):
-    # your code here
-    return
+    if count < 10:
+        return 'Number of donuts: ' + str(count)
+    return 'Number of donuts: many'
 
 
 # B. both_ends
@@ -36,8 +37,9 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-    # your code here
-    return
+    if len(s) < 2:
+        return ''
+    return s[:2] + s[-2:]
 
 
 # C. fix_start
@@ -50,9 +52,7 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-    # your code here
-    return
-
+    return s[0] + s.replace(s[0], '*')[1:]
 
 # D. MixUp
 # Given strings a and b, return a single string with a and b separated
@@ -62,8 +62,7 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-    # your code here
-    return
+    return b[:2] + a[2:] + " " + a[:2] + b[2:]
 
 
 # Provided simple test() function used in main() to print
